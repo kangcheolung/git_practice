bf5f8b05913aa0e4a022c5215564cd791d89174b
kangcheolung <skhu202114111@gmail.com>
2025-04-09 05:26:28 +0900

    fifth

--- src/a1/World.java
+++ src/a1/World.java
@@ -2,4 +2,7 @@ public class World {
   public static void run() {
       System.out.println("201514099");  // 자신의 학번으로 변경
   }
+  public static void run2() {
+      System.out.println("hello world");
+  }
 }
