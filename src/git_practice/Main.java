4c7084616c66bbfe96e96c45c46b32eaaaa22577
kangcheolung <skhu202114111@gmail.com>
2025-04-20 14:55:09 +0900

    three

--- src/git_practice/Main.java
+++ src/git_practice/Main.java
@@ -3,5 +3,6 @@ package git_practice;
 public class Main {
     public static void main(String[] args) {
         System.out.println("홍길동");
+        World.run();
     }
 }
---
+++ src/git_practice/World.java
@@ -0,0 +1,7 @@
+package git_practice;
+
+public class World {
+  public static void run() {
+      System.out.println("201514099");
+  }
+}
